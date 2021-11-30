ea619f85dd830d3b75953053e0d6306cd48c3304
kunalsuryawanshi <kunalsuryawanshi25@gmail.com>
2021-11-30 23:23:14 +0530

    Added: GET Request Method and Pass Name as Query Parameter, REST Call to Show Hello Kunal

--- src/main/java/com/bridgelabz/hellomessagingapp/controller/MessagingController.java
+++ src/main/java/com/bridgelabz/hellomessagingapp/controller/MessagingController.java
@@ -11,4 +11,10 @@ public class MessagingController {
     public String sayHello() {
         return "Hello From BridgeLabz!!!";
     }
+
+    //http://localhost:8080/hello/query?name=Kunal
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
+    }
 }
